becd3bedd217ccb9a75db7e1abe60070184a9b61
Andy Lai <andy51002000@gmail.com>
2017-08-16 15:33:54 +0100

    checking and installing node modules

--- start.ps1
+++ start.ps1
@@ -1,16 +1,26 @@
 # requires: Nodejs/NPM, PowerShell
 # Permission to run PS scripts (for this session only):
-# Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+# Set-ExecutionPolicy RemoteSigned
+# Set-ExecutionPolicy ByPass -File start.ps1
 # exit if cmdlet gives error
-$ErrorActionPreference = "Stop"
+
+
+
+# $ErrorActionPreference = "Stop"
 
 if( !(test-path ./node_modules)){
-    npm install --global --production windows-build-tools
-    npm install
+
+    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        write-Warning "This setup needs admin permissions. Please run this file as admin."     
+        break
+    }
+    start-process -file npm -argumentlist 'install', '--global', '--production', 'windows-build-tools' -wait   | Wait-Process  
+    start-process -file npm -argumentlis 'install' -wait | Wait-Process
 
 }
 
 
 "`nRunning pub/sub sample application..."
-node ./app.js
+node  './app.js' 
